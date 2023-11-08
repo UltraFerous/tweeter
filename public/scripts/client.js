155ad2fce2b8db54d1ef483ed9d8689ee3ff4991
UltraFerous <jandoga31@gmail.com>
2023-11-07 19:27:38 -0500

    Added tweet CSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("READY!");
+
+  $("article").hover(function() {
+    $(this).css('box-shadow', '10px 10px 0px #586ebc');
+  }, function() {
+    $(this).css('box-shadow', '0px 0px 0px #586ebc');
+  });
+
+  $(".report").hover(function() {
+    $(this).css('color', '#BC586E');
+  }, function() {
+    $(this).css('color', '#586ebc');
+  });
+
+  $(".retweet").hover(function() {
+    $(this).css('color', '#BC586E');
+  }, function() {
+    $(this).css('color', '#586ebc');
+  });
+
+  $(".like").hover(function() {
+    $(this).css('color', '#BC586E');
+  }, function() {
+    $(this).css('color', '#586ebc');
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
   // --- our code goes here ---
   console.log("READY!");
+
+  $("#tweet-text").keyup(function(e) {
+    // console.log(e.originalEvent.key);
+    // console.log(140 - this.textLength);
+
+    // target the ancestor
+    const pageSection = $(this).closest('.new-tweet');
+    // target the descendant with a class of tals
+    const counterSection = pageSection.find('.counter');
+
+    // counterSection = 140 - this.textLength;
+    counterSection[0].innerHTML = (140 - this.textLength);
+
+    if (140 - this.textLength < 0) {
+      counterSection.css('color', '#BC586E');;
+    }
+
+    if (140 - this.textLength >= 0) {
+      counterSection.css('color', '#586ebc');;
+    }
+  });
+
 });
+
